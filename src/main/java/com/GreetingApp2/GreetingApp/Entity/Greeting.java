83fbdea07406c4bf7260cd6b3c65b37a0f18f309
puneet0410 <puneetp2200@gmail.com>
2025-03-08 15:16:28 +0530

    UC_5_FindByID

--- src/main/java/com/GreetingApp2/GreetingApp/Entity/Greeting.java
+++ src/main/java/com/GreetingApp2/GreetingApp/Entity/Greeting.java
@@ -2,24 +2,32 @@ package com.GreetingApp2.GreetingApp.Entity;
 
 
 
-import jakarta.persistence.*;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 @Entity
 public class Greeting {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String message;
 
+    // Constructors
     public Greeting() {}
 
     public Greeting(String message) {
         this.message = message;
     }
 
+    // Getters and Setters
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getMessage() {
