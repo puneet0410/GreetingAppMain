166b5e881e671ea22433703db1abadc8f0c349fa
puneet0410 <puneetp2200@gmail.com>
2025-03-08 14:30:11 +0530

    Initial Commit

---
+++ src/test/java/com/GreetingApp2/GreetingApp/GreetingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GreetingApp2.GreetingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
