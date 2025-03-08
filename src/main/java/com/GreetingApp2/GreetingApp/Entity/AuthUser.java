452fb71c2c78726e78cd6e095622819036b72ba0
puneet0410 <puneetp2200@gmail.com>
2025-03-08 15:51:42 +0530

    UC_9_User_Registration

---
+++ src/main/java/com/GreetingApp2/GreetingApp/Entity/AuthUser.java
@@ -0,0 +1,24 @@
+package com.GreetingApp2.GreetingApp.Entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class AuthUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
